c1d913ef56f960d74dedeeca7cde672e423fe060
djchie <djchie.dev@gmail.com>
2015-08-21 10:55:23 -0700

    re-implemented hashTable with arrays instead of objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,10 +9,19 @@ HashTable.prototype.insert = function(k, v){
 
   var bucket = this._storage.get(i);
   if (bucket === undefined){
-    bucket = {};
+    bucket = [];
+    this._storage.set(i, bucket);
   }
-  bucket[k] = v;
-  this._storage.set(i, bucket);
+  var hasValue = false;
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+      hasValue = true;
+    }
+  }
+  if (!hasValue) {
+    bucket.push([k,v]);
+  }
 };
 
 // Complexity: O(1)
@@ -22,9 +31,9 @@ HashTable.prototype.retrieve = function(k){
   if (bucket === undefined) {
     return null;
   }
-  for (var key in bucket) {
-    if (key === k) {
-      return bucket[key];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
     }
   }
   return null;
@@ -37,47 +46,13 @@ HashTable.prototype.remove = function(k){
   if (bucket === undefined) {
     return false;
   }
-  for (var key in bucket) {
-    if (key === k) {
-      delete bucket[key];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i,1);
       return true;
     }
   }
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
-
-// Fails the collision test
-/*
-var HashTable = function(){
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-};
-
-// Complexity: O(1)
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
-};
-
-// Complexity: O(1)
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
-};
-
-// Complexity: O(1)
-HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
-};
-*/
 
 
 /*
