84157fbda759ac5113932e3c79b9d735b48a24d1
djchie <djchie.dev@gmail.com>
2015-08-21 20:14:11 -0700

    Started on the rebalancing of the binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -3,24 +3,37 @@ var BinarySearchTree = function(value){
   newBSTree.value = value;
   newBSTree.left = null;
   newBSTree.right = null;
+  // newBSTree.depth = 0;
   return newBSTree;
 };
+
+// Max depth is the furthest path from root to leaf
+// Min depth 
+// Ex. min depth of Binary tree {1,2} is 2
+// Rebalance when max depth is more than twice the min depth
 
 // Complexity: O(log(n))
 BinarySearchTree.prototype.insert = function(value){
   if (value < this.value){
     if (this.left === null) {
       this.left = new BinarySearchTree(value);
+      // this.left.depth += this.depth + 1;
     } else {
       this.left.insert(value);
     }
   } else if (value > this.value) {
     if (this.right === null) {
       this.right = new BinarySearchTree(value);
+      // this.right.depth += this.depth + 1;
     } else {
       this.right.insert(value);
     }
   }
+
+  // Do a check for minimum depth
+  // Do a check for maximum path
+  // If mex depth is more than twice the min depth
+  // --> rebalance
 }
 
 // Complexity: O(log(n))
@@ -45,7 +58,7 @@ BinarySearchTree.prototype.contains = function(value){
   return hasValue;
 }
 
-// Complexity: O(log(n))
+// Complexity: O(n)
 BinarySearchTree.prototype.depthFirstLog = function(callback){
   callback(this.value);
   if (this.left !== null) {
@@ -56,7 +69,7 @@ BinarySearchTree.prototype.depthFirstLog = function(callback){
   }
 }
 
-// Complexity: O(log(n))
+// Complexity: O(n)
 BinarySearchTree.prototype.breadthFirstLog = function(callback){
   var queue = [];
   queue.push(this);
