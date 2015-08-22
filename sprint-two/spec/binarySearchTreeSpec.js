5115750235da0f67cbf6ace4f25564ccc2e25cfa
djchie <djchie.dev@gmail.com>
2015-08-21 18:24:08 -0700

    Added tests for breadthFirstLog

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -36,4 +36,18 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5,2,3]);
   });
+
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function(){
+    var array = [];
+    var func = function(value){ array.push(value); };
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(9);
+    binarySearchTree.breadthFirstLog(func);
+    expect(array).to.eql([5,3,6,1,8,7,9]);
+  });
+
 });
